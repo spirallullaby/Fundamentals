bd83d30ffee41894bc04d4a075d497fe7fd2c45c
rstefanova <ralicatst@gmail.com>
2017-08-25 23:27:40 +0300

    Add stack

--- PracticeFundamentals/Program.cs
+++ PracticeFundamentals/Program.cs
@@ -10,6 +10,12 @@ namespace PracticeFundamentals
     {
         static void Main(string[] args)
         {
+            TestQueue();
+            TestStack();
+        }
+
+        private static void TestQueue()
+        {
             var queue = new Queue();
             queue.Enqueue("First");
             queue.Enqueue("Second");
@@ -18,7 +24,22 @@ namespace PracticeFundamentals
             var count = queue.Count;
             for (int i = 0; i < count; i++)
             {
-               Console.WriteLine(queue.Dequeue());
+                Console.WriteLine(queue.Dequeue());
+            }
+            Console.ReadLine();
+        }
+
+        private static void TestStack()
+        {
+            var stack = new Stack();
+            stack.Push("1");
+            stack.Push("2");
+            stack.Push("3");
+            stack.Push("4");
+            var cnt = stack.Count;
+            for (int i = 0; i < cnt; i++)
+            {
+                Console.WriteLine(stack.Pop());
             }
             Console.ReadLine();
         }
---
+++ PracticeFundamentals/Stack.cs
@@ -0,0 +1,33 @@
+﻿namespace PracticeFundamentals
+{
+    public class LinkedNode
+    {
+        public LinkedNode(string data)
+        {
+            this.Data = data;
+        }
+        public LinkedNode Previous;
+        public string Data;
+    }
+    public class Stack
+    {
+        private LinkedNode tail;
+        public int Count = 0;
+        public void Push(string data)
+        {
+            var node = new LinkedNode(data);
+            if (tail != null)
+                node.Previous = tail;
+            tail = node;
+            Count++;
+        }
+
+        public string Pop()
+        {
+            var result = tail.Data;
+            tail = tail.Previous;
+            Count--;
+            return result;
+        }
+    }
+}
