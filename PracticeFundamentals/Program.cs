b1aef250d21d05a383d65424873673b9fbe64f8a
spiral <ralicatst@gmail.com>
2017-08-25 10:49:59 +0300

    Queue

---
+++ PracticeFundamentals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue();
+            queue.Enqueue("First");
+            queue.Enqueue("Second");
+            queue.Enqueue("Third");
+            queue.Enqueue("Fourth");
+            var count = queue.Count;
+            for (int i = 0; i < count; i++)
+            {
+               Console.WriteLine(queue.Dequeue());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PracticeFundamentals/Queue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeFundamentals
+{
+    class LinkedListNode
+    {
+        public string Data
+        {
+            get; set;
+        }
+        public LinkedListNode NextNode
+        {
+            get; set;
+        }
+    }
+    class Queue
+    {
+        LinkedListNode head;
+        LinkedListNode tail;
+        public int Count
+        {
+            private set;
+            get;
+        } = 0;
+        public void Enqueue(string data)
+        {
+            var newNode = new LinkedListNode()
+            {
+                Data = data,
+            };
+            if (Count == 0)
+            {
+                head = newNode;
+            }
+            else
+            {
+                tail.NextNode = newNode;
+            }
+            tail = newNode;
+            Count++;
+        }
+        public string Dequeue()
+        {
+            var result = head.Data;
+            head = head.NextNode;
+            Count--;
+            return result;
+        }
+    }
+}
